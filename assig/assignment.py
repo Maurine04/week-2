2a5ccecb97a58358804b35021a3fe34d40be726e
Maurine04 <maurinemwangi999@gmail.com>
2025-04-07 10:52:57 +0300

    Add files via upload

---
+++ assig/assignment.py
@@ -0,0 +1,25 @@
+# Create an empty list
+my_list = []
+# Append elements to the list
+my_list.append(10,20,30,40)
+#to inert no 15 in the index 1
+my_list.insert[1]=15
+# Extend the list with [50, 60, 70]
+my_list.extend([50, 60, 70])
+# Remove the last element
+del my_list[-1]
+# Sort the list in ascending order
+my_list.sort()
+# Find and print the index of the value 30
+print(my_list.index(30))
+
+
+
+
+
+
+
+
+
+
+
